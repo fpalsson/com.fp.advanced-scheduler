cfd251a788389941c430c8e8efdf9217f862e2c3
fpalsson <fredrik.palsson@home.se>
2021-02-20 20:44:56 +0100

    Settings updated
    Almost done with settings page

--- settings/websettings.ts
+++ settings/websettings.ts
@@ -147,7 +147,26 @@ class WebSettings {
         return this.schedules.length;
 
     }
-  
+ 
+    getSunTimes(){
+        return [ new TimeInfo("dawn","Dawn"),
+            new TimeInfo("dusk","Dusk" ),
+            new TimeInfo("goldenHour","Golden Hour" ),
+            new TimeInfo("goldenHourEnd","Golden Hour End" ),
+            new TimeInfo("nadir","Nadir" ),
+            new TimeInfo("nauticalDawn","Nautical Dawn" ),
+            new TimeInfo("nauticalDusk","Nautical Dusk" ),
+            new TimeInfo("night","Night" ),
+            new TimeInfo("nightEnd","Night End" ),
+            new TimeInfo("solarNoon","Solar Noon" ),
+            new TimeInfo("sunrise","Sunrise" ),
+            new TimeInfo("sunriseEnd","Sunrise End" ),
+            new TimeInfo("sunset","Sunset" ),
+            new TimeInfo("sunsetStart","Sunset Start" ),
+        ]
+    }
+
+
 }
 
 class Schedule {
@@ -193,7 +212,14 @@ enum TimeType {
     Solar = 2
 }
 
-
+class Day{
+    constructor (day:string, value:number){
+        this.day=day;
+        this.value=value;
+    }
+    day:string;
+    value:number;
+}
 
 class ScheduleItem {
     constructor(id:number,
@@ -202,6 +228,11 @@ class ScheduleItem {
                 timetype:TimeType,
                 suneventtype:string,
                 timearg:string){
+
+        this.allDays = new Array();
+        this.allDays=this.getAllDays();
+        this.internalSelectedDays=new Array();
+
         this.id=id;
         this.daystype=daytype;
         this.daysarg=daysarg;
@@ -211,35 +242,123 @@ class ScheduleItem {
         this.tokenitems = new Array();
         this.deletedialogopen=false;
         this.editdialogopen=false;
+        this.updateSelectedDays();
     }
 
     id:number;
     daystype:DaysType;
-    daysarg:number // 0..127, monday is 1, tuesday is 2, wednesday is 4, thursday is 8 and so on;
+    internaldaysarg:number // 0..127, monday is 1, tuesday is 2, wednesday is 4, thursday is 8 and so on;
     timetype:TimeType;
     suneventtype:string;
     timearg:string;
     tokenitems:TokenItem[];
     deletedialogopen:boolean;
     editdialogopen:boolean;
+    private allDays:Day[];
+    private internalSelectedDays:Day[];
 
-    get daysArgText(){
-        if (this.daysarg == 0) return "Nothing";
-        else if (this.daysarg == 1+2+4+8+16) return "Weekdays";
-        else if (this.daysarg == 32+64) return "Weekends";
-        else if (this.daysarg == 1+2+4+8+16+32+64) return "All week";
+    getAllDays(){
+        if (this.allDays.length > 0) return this.allDays;
+
+        var mo = new Day("Monday", 1);
+        var tu = new Day("Tuesday", 2);
+        var we = new Day("Wednesday", 4);
+        var th = new Day("Thursday", 8);
+        var fr = new Day("Friday", 16);
+        var sa = new Day("Saturday", 32);
+        var su = new Day("Sunday", 64);
+
+        this.allDays.push(mo, tu, we, th, fr, sa, su);
+
+        return this.allDays;
+    }
+
+    get daysarg():number{
+        return this.internaldaysarg;
+    }
+
+    set daysarg(value:number){
+        console.log('set daysarg');
+        this.internaldaysarg=value;
+        console.log('set daysarg before update');
+        this.updateSelectedDays();
+        console.log('set daysarg updated');
+    }
+
+
+    private updateSelectedDays(){
+        //This takes care to not add or remove elements that are not needed to be added or removed, to make Vue work as expected!!!
+        console.log('updateSelectedDays current number: ' + this.internalSelectedDays.length);
+
+        for (let i = 0; i < 7; i++)
+        {
+            let val:number = Math.pow(2,i);
+            let shouldexist:boolean = (val & this.daysarg) > 0;
+            let foundday:Day=null;
+            this.internalSelectedDays.forEach(d => {
+                if (d.value==val) foundday = d; 
+            })
+
+            console.log('updateSelectedDays val:' + val + ' shouldexist:'+ shouldexist + ' foundday:' + foundday);
+
+            if (shouldexist && foundday == null) {
+                //add
+                this.internalSelectedDays.push(this.allDays[i]);
+                console.log('updateSelectedDays added' + this.allDays[i].day + this.allDays[i].value);
+            }
+            if (!shouldexist && foundday != null) {
+                //remove
+                this.internalSelectedDays.splice(this.internalSelectedDays.indexOf(foundday),1);
+                console.log('updateSelectedDays removed ' + foundday.day );
+            }
+        }
+
+        console.log('updateSelectedDays' + this.internalSelectedDays);
+
+    }
+
+    get selectedDays():Day[]{
+        console.log('get selectedDays'+ this.internalSelectedDays);
+
+        return this.internalSelectedDays;
+    }
+
+    set selectedDays(value:Day[]){
+        console.log('set selectedDays' + value);
+        let daysarg:number=0;
+        value.forEach(day=> {
+            daysarg+=day.value;
+            console.log('day' + day.value)
+        })
+        console.log('days' + daysarg)
+        this.daysarg=daysarg;
+    }
+
+    //Not so beutiful to have presentation logic here, but...
+    get daysArgShortText():string {
+        let da:number = this.daysarg;
+        console.log('daysArgShortText daysarg: ' + da)
+        if (da == 0) return "Nothing";
+        else if (da == 1+2+4+8+16) return "Weekdays";
+        else if (da == 32+64) return "Weekends";
+        else if (da == 1+2+4+8+16+32+64) return "All week";
         else{
+            //console.log('daysArgShortText daysarg2: ' + da)
             var s = '';
-            if (this.daysarg && 1 > 0) s+='Mo,';
-            if (this.daysarg && 2 > 0) s+='Tu,';
-            if (this.daysarg && 4 > 0) s+='We,';
-            if (this.daysarg && 8 > 0) s+='Th,';
-            if (this.daysarg && 16 > 0) s+='Fr,';
-            if (this.daysarg && 32 > 0) s+='Sa,';
-            if (this.daysarg && 64 > 0) s+='Su,';
+            if ((da & 1) > 0) s+='Mo,';
+            if ((da & 2) > 0) s+='Tu,';
+            if ((da & 4) > 0) s+='We,';
+            if ((da & 8) > 0) s+='Th,';
+            if ((da & 16) > 0) s+='Fr,';
+            if ((da & 32) > 0) s+='Sa,';
+            if ((da & 64) > 0) s+='Su,';
+            //console.log('daysArgShortText daysarg3: ' + s)
+            s = s.substring(0, s.length-1);
             return s;
         }
     }
+
+
 }
 
 class TokenItem {
@@ -254,6 +373,16 @@ class TokenItem {
     deletedialogopen:boolean;
 }
 
+class TimeInfo{
+    constructor(id:string,desc:string){//,time:Date){
+        this.id = id;
+        this.desc = desc;
+  //      this.time = time;
+    }
 
+    id:string;
+    desc:string;
+//    time:Date;
+}
   
 
